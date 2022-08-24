a6e3c74789b64050c6175caca6461e7afef6d9d6
olgadankova <olga.dankova94@gmail.com>
2022-08-24 12:51:31 +0200

    fix(components): remove styles for a component MainTemplate

--- src/components/MainTemplate/MainTemplate.tsx
+++ src/components/MainTemplate/MainTemplate.tsx
@@ -1,13 +1,12 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import { Nav } from "../Nav/Nav";
-import { Container } from "./styles";
 
 export const MainTemplate = () => {
   return (
-    <Container>
+    <>
       <Nav />
       <Outlet />
-    </Container>
+    </>
   );
 };
