7e6c6886a345744603522176b950f72c5272c719
olgadankova <olga.dankova94@gmail.com>
2022-08-29 16:48:20 +0200

    refactor(ui): fix GlobalStyles

--- src/ui/GlobalStyles.ts
+++ src/ui/GlobalStyles.ts
@@ -1,11 +1,19 @@
 import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyles = createGlobalStyle`
-*, *::before, *::after {
-  margin: 0;
-  padding: 0; 
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+button {
+  font-family: inherit;
+  border: 0;
+  background: none;
+  outline: none;
+}
+body {
   font-family: 'Inter', sans-serif;
-  box-sizing: border-box;
-  text-decoration: none;
 }
 `;
+export default GlobalStyles;
