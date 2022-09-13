cf4c2e6a84169e58d04b1e9553fc1579a929efc7
olgadankova <olga.dankova94@gmail.com>
2022-09-13 22:44:50 +0200

    init(hooks): add hook useToggle

---
+++ src/hooks/useToggle.ts
@@ -0,0 +1,13 @@
+import { useCallback, useState } from "react";
+
+export const useToggle = (
+  initialValue: boolean = false
+): [boolean, () => void] => {
+  const [value, setValue] = useState<boolean>(initialValue);
+
+  const toggle = useCallback(() => {
+    setValue((value) => !value);
+  }, []);
+
+  return [value, toggle];
+};
